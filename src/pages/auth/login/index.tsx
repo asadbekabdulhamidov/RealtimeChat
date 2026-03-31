8c5639dbb3c8d8c2b3c4b99fcd8f147953cc5a15
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2026-03-31 11:53:38 +0500

    passwordn input placeholder add remove degaultValue

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -60,7 +60,7 @@ const LoginPage = () => {
             <div className="relative">
               <input
                 type={showPassword ? 'text' : 'password'}
-                defaultValue="wrongpassword123"
+                placeholder="Enter your password"
                 className="w-full mt-1 border border-red-300 rounded-lg px-3 py-2 pr-10 outline-none focus:ring-2 focus:ring-indigo-500"
               />
 
@@ -68,7 +68,7 @@ const LoginPage = () => {
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
+                className="absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
               >
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </button>
@@ -82,7 +82,7 @@ const LoginPage = () => {
           </div>
 
           {/* BUTTON */}
-          <Button className="w-full bg-gradient-to-r from-indigo-500 to-purple-500 text-white">
+          <Button className="w-full bg-gradient-to-r cursor-pointer from-indigo-500 to-purple-500 text-white">
             Login
           </Button>
         </form>
